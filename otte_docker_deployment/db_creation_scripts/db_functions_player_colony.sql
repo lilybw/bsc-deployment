947e1a7863761af140113c840da349cd3e5dad74
lilybw <lilybwanscher@gmail.com>
2024-08-22 22:16:12 +0200

    All table names and database name succesfully escaped
    Apparently, postgres respects env vars, but does not respect casing anywhere else. Do be aware.

--- otte_docker_deployment/db_creation_scripts/db_functions_player_colony.sql
+++ otte_docker_deployment/db_creation_scripts/db_functions_player_colony.sql
@@ -8,14 +8,14 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.assetCollection IS NOT NULL THEN
         -- Case 1: If the assetCollection is not null, append the new id to the entries array
-        UPDATE AssetCollection
+        UPDATE "AssetCollection"
         SET collectionEntries = array_append(collectionEntries, NEW.id)
         WHERE id = NEW.assetCollection;
     END IF;
     
     IF OLD.assetCollection IS NOT NULL AND NEW.assetCollection IS DISTINCT FROM OLD.assetCollection THEN
         -- Case 2: If the old assetCollection is not null and differs from the new one, remove the id from the old collection
-        UPDATE AssetCollection
+        UPDATE "AssetCollection"
         SET collectionEntries = array_remove(collectionEntries, OLD.id)
         WHERE id = OLD.assetCollection;
     END IF;
@@ -26,7 +26,7 @@ $$ LANGUAGE plpgsql;
 
 -- Step 2: Create or replace the trigger to invoke this function after insert, update, or delete
 CREATE TRIGGER collectionentry_insert_update_trigger
-AFTER INSERT OR UPDATE ON CollectionEntry
+AFTER INSERT OR UPDATE ON "CollectionEntry"
 FOR EACH ROW
 EXECUTE FUNCTION update_assetcollection_entries();
 
@@ -36,7 +36,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF OLD.assetCollection IS NOT NULL THEN
         -- Remove the entry from the array when the entry is deleted
-        UPDATE AssetCollection
+        UPDATE "AssetCollection"
         SET collectionEntries = array_remove(collectionEntries, OLD.id)
         WHERE id = OLD.assetCollection;
     END IF;
@@ -46,7 +46,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER collectionentry_delete_trigger
-AFTER DELETE ON CollectionEntry
+AFTER DELETE ON "CollectionEntry"
 FOR EACH ROW
 EXECUTE FUNCTION delete_collectionentry_trigger();
 ---------------------------------- Update AssetCollection Entries ----------------------------------
@@ -56,7 +56,7 @@ CREATE OR REPLACE FUNCTION sync_collectionentry_addition()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Update the CollectionEntry to point to the AssetCollection
-    UPDATE CollectionEntry
+    UPDATE "CollectionEntry"
     SET assetCollection = NEW.id
     WHERE id = ANY(NEW.collectionEntries);
     
@@ -65,7 +65,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_assetcollection_update_addition
-AFTER UPDATE OF collectionEntries ON AssetCollection
+AFTER UPDATE OF collectionEntries ON "AssetCollection"
 FOR EACH ROW
 WHEN (NEW.collectionEntries IS DISTINCT FROM OLD.collectionEntries)
 EXECUTE FUNCTION sync_collectionentry_addition();
@@ -74,7 +74,7 @@ CREATE OR REPLACE FUNCTION sync_collectionentry_removal()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Set the CollectionEntry assetCollection field to NULL if the ID is removed
-    DELETE FROM CollectionEntry 
+    DELETE FROM "CollectionEntry" 
     WHERE id = ANY(OLD.collectionEntries)
       AND id != ALL(NEW.collectionEntries);
     
@@ -83,7 +83,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_assetcollection_update_removal
-AFTER UPDATE OF collectionEntries ON AssetCollection
+AFTER UPDATE OF collectionEntries ON "AssetCollection"
 FOR EACH ROW
 WHEN (NEW.collectionEntries IS DISTINCT FROM OLD.collectionEntries)
 EXECUTE FUNCTION sync_collectionentry_removal();
@@ -95,7 +95,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.colony IS NOT NULL THEN
         -- Case 1: Add the `ColonyLocation` ID to the `locations` array in `Colony`
-        UPDATE Colony
+        UPDATE "Colony"
         SET locations = array_append(locations, NEW.id)
         WHERE id = NEW.colony;
     END IF;
@@ -112,7 +112,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER colonylocation_insert_update_trigger
-AFTER INSERT OR UPDATE ON ColonyLocation
+AFTER INSERT OR UPDATE ON "ColonyLocation"
 FOR EACH ROW
 EXECUTE FUNCTION update_colony_locations();
 
@@ -121,7 +121,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF OLD.colony IS NOT NULL THEN
         -- Remove the `ColonyLocation` ID from the `locations` array in `Colony`
-        UPDATE Colony
+        UPDATE "Colony"
         SET locations = array_remove(locations, OLD.id)
         WHERE id = OLD.colony;
     END IF;
@@ -131,7 +131,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER colonylocation_delete_trigger
-AFTER DELETE ON ColonyLocation
+AFTER DELETE ON "ColonyLocation"
 FOR EACH ROW
 EXECUTE FUNCTION delete_colonylocation_trigger();
 ---------------------------------- Update Colony (Locations) ----------------------------------
@@ -141,7 +141,7 @@ CREATE OR REPLACE FUNCTION sync_colonylocation_addition()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Update `ColonyLocation` to point to the `Colony`
-    UPDATE ColonyLocation
+    UPDATE "ColonyLocation"
     SET colony = NEW.id
     WHERE id = ANY(NEW.locations);
     
@@ -150,7 +150,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_update_addition_locations
-AFTER UPDATE OF locations ON Colony
+AFTER UPDATE OF locations ON "Colony"
 FOR EACH ROW
 WHEN (NEW.locations IS DISTINCT FROM OLD.locations)
 EXECUTE FUNCTION sync_colonylocation_addition();
@@ -159,7 +159,7 @@ CREATE OR REPLACE FUNCTION sync_colonylocation_removal()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Set the `ColonyLocation` `colony` field to NULL if the ID is removed
-    UPDATE ColonyLocation
+    UPDATE "ColonyLocation"
     SET colony = NULL
     WHERE id = ANY(OLD.locations)
       AND id != ALL(NEW.locations);
@@ -169,7 +169,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_update_removal_locations
-AFTER UPDATE OF locations ON Colony
+AFTER UPDATE OF locations ON "Colony"
 FOR EACH ROW
 WHEN (NEW.locations IS DISTINCT FROM OLD.locations)
 EXECUTE FUNCTION sync_colonylocation_removal();
@@ -181,14 +181,14 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF NEW.colony IS NOT NULL THEN
         -- Case 1: Add the `ColonyAsset` ID to the `assets` array in `Colony`
-        UPDATE Colony
+        UPDATE "Colony"
         SET assets = array_append(assets, NEW.id)
         WHERE id = NEW.colony;
     END IF;
     
     IF OLD.colony IS NOT NULL AND NEW.colony IS DISTINCT FROM OLD.colony THEN
         -- Case 2: Remove the `ColonyAsset` ID from the old `Colony`
-        UPDATE Colony
+        UPDATE "Colony"
         SET assets = array_remove(assets, OLD.id)
         WHERE id = OLD.colony;
     END IF;
@@ -198,7 +198,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER colonyasset_insert_update_trigger
-AFTER INSERT OR UPDATE ON ColonyAsset
+AFTER INSERT OR UPDATE ON "ColonyAsset"
 FOR EACH ROW
 EXECUTE FUNCTION update_colony_assets();
 
@@ -207,7 +207,7 @@ RETURNS TRIGGER AS $$
 BEGIN
     IF OLD.colony IS NOT NULL THEN
         -- Remove the `ColonyAsset` ID from the `assets` array in `Colony`
-        UPDATE Colony
+        UPDATE "Colony"
         SET assets = array_remove(assets, OLD.id)
         WHERE id = OLD.colony;
     END IF;
@@ -217,7 +217,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER colonyasset_delete_trigger
-AFTER DELETE ON ColonyAsset
+AFTER DELETE ON "ColonyAsset"
 FOR EACH ROW
 EXECUTE FUNCTION delete_colonyasset_trigger();
 ---------------------------------- Update Colony (Assets) ----------------------------------
@@ -227,7 +227,7 @@ CREATE OR REPLACE FUNCTION sync_colonyasset_addition()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Update `ColonyAsset` to point to the `Colony`
-    UPDATE ColonyAsset
+    UPDATE "ColonyAsset"
     SET colony = NEW.id
     WHERE id = ANY(NEW.assets);
     
@@ -236,7 +236,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_update_addition_assets
-AFTER UPDATE OF assets ON Colony
+AFTER UPDATE OF assets ON "Colony"
 FOR EACH ROW
 WHEN (NEW.assets IS DISTINCT FROM OLD.assets)
 EXECUTE FUNCTION sync_colonyasset_addition();
@@ -245,7 +245,7 @@ CREATE OR REPLACE FUNCTION sync_colonyasset_removal()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Set the `ColonyAsset` `colony` field to NULL if the ID is removed
-    UPDATE ColonyAsset
+    UPDATE "ColonyAsset"
     SET colony = NULL
     WHERE id = ANY(OLD.assets)
       AND id != ALL(NEW.assets);
@@ -255,7 +255,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_update_removal_assets
-AFTER UPDATE OF assets ON Colony
+AFTER UPDATE OF assets ON "Colony"
 FOR EACH ROW
 WHEN (NEW.assets IS DISTINCT FROM OLD.assets)
 EXECUTE FUNCTION sync_colonyasset_removal();
@@ -266,7 +266,7 @@ CREATE OR REPLACE FUNCTION sync_colony_update()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Sync ColonyCode's colony when Colony is updated
-    UPDATE ColonyCode
+    UPDATE "ColonyCode"
     SET colony = NEW.id
     WHERE id = NEW.colonyCode;
 
@@ -275,7 +275,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_update
-AFTER INSERT OR UPDATE ON Colony
+AFTER INSERT OR UPDATE ON "Colony"
 FOR EACH ROW
 EXECUTE FUNCTION sync_colony_update();
 
@@ -283,7 +283,7 @@ CREATE OR REPLACE FUNCTION handle_colony_deletion()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Set ColonyCode's colony to NULL when corresponding Colony is deleted
-    UPDATE ColonyCode
+    UPDATE "ColonyCode"
     SET colony = NULL
     WHERE colony = OLD.id;
 
@@ -292,7 +292,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colony_delete
-AFTER DELETE ON Colony
+AFTER DELETE ON "Colony"
 FOR EACH ROW
 EXECUTE FUNCTION handle_colony_deletion();
 ---------------------------------- Update ColonyCode ----------------------------------
@@ -302,7 +302,7 @@ CREATE OR REPLACE FUNCTION sync_colonycode_update()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Sync Colony's colonyCode when ColonyCode is updated
-    UPDATE Colony
+    UPDATE "Colony"
     SET colonyCode = NEW.id
     WHERE id = NEW.colony;
 
@@ -311,7 +311,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colonycode_update
-AFTER INSERT OR UPDATE ON ColonyCode
+AFTER INSERT OR UPDATE ON "ColonyCode"
 FOR EACH ROW
 EXECUTE FUNCTION sync_colonycode_update();
 
@@ -319,7 +319,7 @@ CREATE OR REPLACE FUNCTION handle_colonycode_deletion()
 RETURNS TRIGGER AS $$
 BEGIN
     -- Set Colony's colonyCode to NULL when corresponding ColonyCode is deleted
-    UPDATE Colony
+    UPDATE "Colony"
     SET colonyCode = NULL
     WHERE colonyCode = OLD.id;
 
@@ -328,7 +328,7 @@ END;
 $$ LANGUAGE plpgsql;
 
 CREATE TRIGGER after_colonycode_delete
-AFTER DELETE ON ColonyCode
+AFTER DELETE ON "ColonyCode"
 FOR EACH ROW
 EXECUTE FUNCTION handle_colonycode_deletion();
 ---------------------------------- Update Colony ----------------------------------
@@ -364,7 +364,7 @@ RETURNS TABLE(asset_collection_id INT, asset_collection_name VARCHAR(255)) AS $$
 BEGIN
     RETURN QUERY
     SELECT ac.id, ac.name
-    FROM AssetCollection ac
+    FROM "AssetCollection" ac
     WHERE ac.id = ANY(
         SELECT unnest(l.appearances)
         FROM Location l
--- otte_docker_deployment/db_creation_scripts/db_tables_player_colony.sql
+++ otte_docker_deployment/db_creation_scripts/db_tables_player_colony.sql
@@ -1,7 +1,7 @@
 -- Missing tables: 'PlayerPreferences' and 'AvailablePreference' due to lack of references.
 -- TODO: Does any attributes need to be 'NOT NULL', look at database diagram and discuss.
 
-CREATE TABLE IF NOT EXISTS GraphicalAsset (
+CREATE TABLE IF NOT EXISTS "GraphicalAsset" (
     id SERIAL PRIMARY KEY,
     alias VARCHAR(255) NOT NULL, -- For debugging
     type VARCHAR(255) NOT NULL, -- Assuming the MIME type strings
@@ -12,14 +12,14 @@ CREATE TABLE IF NOT EXISTS GraphicalAsset (
     hasLOD BOOLEAN NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS NPC (
+CREATE TABLE IF NOT EXISTS "NPC" (
     id SERIAL PRIMARY KEY,
     sprite INT,
     name VARCHAR(255),
-    FOREIGN KEY (sprite) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (sprite) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS Transform (
+CREATE TABLE IF NOT EXISTS "Transform" (
     id SERIAL PRIMARY KEY,
     xScale FLOAT DEFAULT 1,
     yScale FLOAT DEFAULT 1,
@@ -28,39 +28,39 @@ CREATE TABLE IF NOT EXISTS Transform (
     zIndex INT DEFAULT 100
 );
 
-CREATE TABLE IF NOT EXISTS AssetCollection (
+CREATE TABLE IF NOT EXISTS "AssetCollection" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) DEFAULT 'DATA.UNNAMED.COLLECTION',
     collectionEntries INT[] DEFAULT '{}'  -- Array of foreign keys pointing to CollectionEntry IDs (array for multiple references and to avoid creation conflicts, triggers will handle)
 );
 
-CREATE TABLE IF NOT EXISTS Achievement (
+CREATE TABLE IF NOT EXISTS "Achievement" (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description TEXT,
     icon INT NOT NULL,
-    FOREIGN KEY (icon) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (icon) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS Player (
+CREATE TABLE IF NOT EXISTS "Player" (
     id SERIAL PRIMARY KEY,
     IGN VARCHAR(255) UNIQUE NOT NULL,
     sprite INT NOT NULL,  
     achievements INT[] DEFAULT '{}', -- Should achievements not just reference player to make a proper one to many relationship?
-    FOREIGN KEY (sprite) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (sprite) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS Session (
+CREATE TABLE IF NOT EXISTS "Session" (
     id SERIAL PRIMARY KEY,
     player INT NOT NULL,
     createdAt TIMESTAMP DEFAULT NOW(), -- Perform check in backend against maxValidDuration.
     token VARCHAR(255) UNIQUE NOT NULL,  -- Unique session token
     validDuration INTERVAL DEFAULT '1 hour',  -- Default session duration
     lastCheckIn TIMESTAMP DEFAULT NOW(),  -- Timestamp of last activity
-    FOREIGN KEY (player) REFERENCES Player(id)
+    FOREIGN KEY (player) REFERENCES "Player"(id)
 );
 
-CREATE TABLE IF NOT EXISTS Colony (
+CREATE TABLE IF NOT EXISTS "Colony" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) DEFAULT 'DATA.UNNAMED.COLONY',
     accLevel INT DEFAULT 0,
@@ -68,83 +68,83 @@ CREATE TABLE IF NOT EXISTS Colony (
     owner INT NOT NULL,
     assets INT[] DEFAULT '{}',  -- Array of foreign keys pointing to ColonyAsset IDs (array for multiple references and to avoid creation conflicts, triggers will handle)
     locations INT[] DEFAULT '{}',  -- Array of foreign keys pointing to ColonyLocation IDs (array for multiple references and to avoid creation conflicts, triggers will handle)
-    FOREIGN KEY (owner) REFERENCES Player(id),
+    FOREIGN KEY (owner) REFERENCES "Player"(id),
     colonyCode INT  -- Single foreign key pointing to ColonyCode ID (Single id attribute to avoid creation conflicts, triggers will handle)
 );
 
-CREATE TABLE IF NOT EXISTS ColonyCode (
+CREATE TABLE IF NOT EXISTS "ColonyCode" (
     id SERIAL PRIMARY KEY,
     lobbyId INT NOT NULL,  -- foreign key? To multiplayer backend.
     serverAddress VARCHAR(255) NOT NULL,
     colony INT NOT NULL,  -- Server per lobby?
     value VARCHAR(6) UNIQUE NOT NULL,
-    FOREIGN KEY (colony) REFERENCES Colony(id)
+    FOREIGN KEY (colony) REFERENCES "Colony"(id)
 );
 
-CREATE TABLE IF NOT EXISTS MiniGame (
+CREATE TABLE IF NOT EXISTS "MiniGame" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) DEFAULT 'DATA.UNNAMED.MINIGAME',
     icon INT NOT NULL,
     description TEXT DEFAULT 'UI.DESCRIPTION_MISSING',
     settings JSON NOT NULL,  -- Assuming settings are stored in JSON format
-    FOREIGN KEY (icon) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (icon) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS MiniGameDifficulty (
+CREATE TABLE IF NOT EXISTS "MiniGameDifficulty" (
     id SERIAL PRIMARY KEY,
     minigame INT NOT NULL,
     icon INT NOT NULL,
     name VARCHAR(2) DEFAULT '?',  -- Roman Numerals as string.
     description TEXT DEFAULT 'UI.DESCRIPTION_MISSING',
     overwritingSettings JSON NOT NULL,  -- Overwrites default settings with these specific to the difficulty level
-    FOREIGN KEY (minigame) REFERENCES MiniGame(id),
-    FOREIGN KEY (icon) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (minigame) REFERENCES "MiniGame"(id),
+    FOREIGN KEY (icon) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS Location (
+CREATE TABLE IF NOT EXISTS "Location" (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) DEFAULT 'DATA.UNNAMED.LOCATION',
     description TEXT DEFAULT 'UI.DESCRIPTION_MISSING',
     minigame INT,
     appearences INT[] DEFAULT '{}', -- Ids of AssetCollections.
-    FOREIGN KEY (minigame) REFERENCES MiniGame(id)
+    FOREIGN KEY (minigame) REFERENCES "MiniGame"(id)
 );
 
-CREATE TABLE IF NOT EXISTS ColonyLocation (
+CREATE TABLE IF NOT EXISTS "ColonyLocation" (
     id SERIAL PRIMARY KEY,
     colony INT NOT NULL,
     location INT NOT NULL,
     transform INT NOT NULL,
     level INT DEFAULT 1,
-    FOREIGN KEY (colony) REFERENCES Colony(id),
-    FOREIGN KEY (location) REFERENCES Location(id),
-    FOREIGN KEY (transform) REFERENCES Transform(id)
+    FOREIGN KEY (colony) REFERENCES "Colony"(id),
+    FOREIGN KEY (location) REFERENCES "Location"(id),
+    FOREIGN KEY (transform) REFERENCES "Transform"(id)
 );
 
-CREATE TABLE IF NOT EXISTS ColonyAsset (
+CREATE TABLE IF NOT EXISTS "ColonyAsset" (
     id SERIAL PRIMARY KEY,
     assetCollection INT NOT NULL,
     transform INT NOT NULL,
     colony INT NOT NULL,
-    FOREIGN KEY (assetCollection) REFERENCES AssetCollection(id),
-    FOREIGN KEY (transform) REFERENCES Transform(id),
-    FOREIGN KEY (colony) REFERENCES Colony(id)
+    FOREIGN KEY (assetCollection) REFERENCES "AssetCollection"(id),
+    FOREIGN KEY (transform) REFERENCES "Transform"(id),
+    FOREIGN KEY (colony) REFERENCES "Colony"(id)
 );
 
-CREATE TABLE IF NOT EXISTS CollectionEntry (
+CREATE TABLE IF NOT EXISTS "CollectionEntry" (
     id SERIAL PRIMARY KEY,
     transform INT NOT NULL,
     assetCollection INT NOT NULL, -- Foreign key to AssetCollection this CollectionEntry is a part of
     graphicalAsset INT NOT NULL,
-    FOREIGN KEY (transform) REFERENCES Transform(id),
-    FOREIGN KEY (assetCollection) REFERENCES AssetCollection(id),
-    FOREIGN KEY (graphicalAsset) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (transform) REFERENCES "Transform"(id),
+    FOREIGN KEY (assetCollection) REFERENCES "AssetCollection"(id),
+    FOREIGN KEY (graphicalAsset) REFERENCES "GraphicalAsset"(id)
 );
 
-CREATE TABLE IF NOT EXISTS LOD (
+CREATE TABLE IF NOT EXISTS "LOD" (
     id SERIAL PRIMARY KEY,
     detailLevel INT DEFAULT 1,
     blob BYTEA NOT NULL,
     graphicalAsset INT NOT NULL,
-    FOREIGN KEY (graphicalAsset) REFERENCES GraphicalAsset(id)
+    FOREIGN KEY (graphicalAsset) REFERENCES "GraphicalAsset"(id)
 );
