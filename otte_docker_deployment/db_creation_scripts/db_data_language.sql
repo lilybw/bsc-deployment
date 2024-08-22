c067d37fc6d8fc473edb6d96aedf2368b47db1ea
lilybw <lilybwanscher@gmail.com>
2024-08-22 22:25:35 +0200

    Language DB enabled

---
+++ otte_docker_deployment/db_creation_scripts/db_data_language.sql
@@ -0,0 +1,8 @@
+ -- INSERT DEFAULT VALUES
+
+INSERT INTO "Catalogue" (key, "EN", "DA") VALUES 
+('DATA.UNNAMED.COLLECTION',     'Unnamed Collection',   'Navneløs Gruppering'), 
+('DATA.UNNAMED.COLONY',         'Unnamed Colony',       'Navneløs Koloni'),
+('DATA.UNNAMED.MINIGAME',       'Unnamed Minigame',     'Navneløst Miniaturespil'),
+('DATA.UNNAMED.LOCATION',       'Unnamed Location',     'Navneløs Lokation'),
+('UI.DESCRIPTION_MISSING',      'Missing Description',  'Manglende Beskrivelse');
--- otte_docker_deployment/db_creation_scripts/db_tables_language.sql
+++ otte_docker_deployment/db_creation_scripts/db_tables_language.sql
@@ -1,89 +1,21 @@
-CREATE TABLE IF NOT EXISTS Catalogue (
+CREATE TABLE IF NOT EXISTS "Catalogue" (
     key TEXT primary key,
-    DA TEXT,
-    NO TEXT,
-    EN TEXT not null
+    "DA" TEXT,
+    "NO" TEXT,
+    "EN" TEXT not null
 );
 
-CREATE TABLE IF NOT EXISTS DA_Wordlist (
+CREATE TABLE IF NOT EXISTS "DA_Wordlist" (
     id serial primary key,
     word VARCHAR(255) not null unique
 );
 
-CREATE TABLE IF NOT EXISTS EN_Wordlist (
+CREATE TABLE IF NOT EXISTS "EN_Wordlist" (
     id serial primary key,
     word VARCHAR(255) not null unique
 );
 
-CREATE TABLE IF NOT EXISTS NO_Wordlist (
+CREATE TABLE IF NOT EXISTS "NO_Wordlist" (
     id serial primary key,
     word VARCHAR(255) not null unique
 );
-
--- ADD EACH WORD OF INSERTED TRANSLATED STRING TO RESPECTIVE WORDLIST
-
-CREATE OR REPLACE FUNCTION add_words_to_da_wordlist() RETURNS trigger AS $$
-DECLARE
-    word TEXT;
-BEGIN
-    FOR word IN SELECT unnest(string_to_array(NEW.DA, ' ')) LOOP
-        INSERT INTO DA_Wordlist(word)
-        VALUES (word)
-        ON CONFLICT DO NOTHING; -- Conflict will rise if word already exists (unique constraint), which is okay and expected
-    END LOOP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION add_words_to_en_wordlist() RETURNS trigger AS $$
-DECLARE
-    word TEXT;
-BEGIN
-    FOR word IN SELECT unnest(string_to_array(NEW.EN, ' ')) LOOP
-        INSERT INTO EN_Wordlist(word)
-        VALUES (word)
-        ON CONFLICT DO NOTHING; -- Conflict will rise if word already exists (unique constraint), which is okay and expected
-    END LOOP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE OR REPLACE FUNCTION add_words_to_no_wordlist() RETURNS trigger AS $$
-DECLARE
-    word TEXT;
-BEGIN
-    FOR word IN SELECT unnest(string_to_array(NEW.NO, ' ')) LOOP
-        INSERT INTO NO_Wordlist(word)
-        VALUES (word)
-        ON CONFLICT DO NOTHING; -- Conflict will rise if word already exists (unique constraint), which is okay and expected
-    END LOOP;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_add_da_words
-AFTER INSERT ON Catalogue
-FOR EACH ROW
-WHEN (NEW.DA IS NOT NULL)
-EXECUTE FUNCTION add_words_to_da_wordlist();
-
-CREATE TRIGGER trigger_add_en_words
-AFTER INSERT ON Catalogue
-FOR EACH ROW
- -- EN has the "not null" constraint so no need to check like the others as translations to some languages might not exist initially
-EXECUTE FUNCTION add_words_to_en_wordlist();
-
-CREATE TRIGGER trigger_add_no_words
-AFTER INSERT ON Catalogue
-FOR EACH ROW
-WHEN (NEW.NO IS NOT NULL)
-EXECUTE FUNCTION add_words_to_no_wordlist();
-
- -- INSERT DEFAULT VALUES
-
-INSERT INTO Catalogue(key, EN, DA) VALUES 
-("DATA.UNNAMED.COLLECTION", "Unnamed Collection", "Navneløs Gruppering"), 
-("DATA.UNNAMED.COLONY", "Unnamed Colony", "Navneløs Koloni"),
-("DATA.UNNAMED.MINIGAME", "Unnamed Minigame", "Navneløst Miniaturespil"),
-("DATA.UNNAMED.LOCATION", "Unnamed Location", "Navneløs Lokation"),
-("UI.DESCRIPTION_MISSING", "Missing Description", "Manglende Beskrivelse");
