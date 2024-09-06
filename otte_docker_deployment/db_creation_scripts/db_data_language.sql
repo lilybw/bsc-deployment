9fbf2b5179bdd20226b68b55d3d0bf15d85a960e
lilybw <lilybwanscher@gmail.com>
2024-09-06 19:28:45 +0200

    Sql bug fix

--- otte_docker_deployment/db_creation_scripts/db_data_language.sql
+++ otte_docker_deployment/db_creation_scripts/db_data_language.sql
@@ -1,12 +1,12 @@
  -- INSERT DEFAULT VALUES
 
-INSERT INTO "Catalogue" (key, "EN", "DA") VALUES 
+INSERT INTO "Catalogue" ("key", "EN", "DA") VALUES 
 ('DATA.UNNAMED.COLLECTION',     'Unnamed Collection',   'Navneløs Gruppering'), 
 ('DATA.UNNAMED.COLONY',         'Unnamed Colony',       'Navneløs Koloni'),
 ('DATA.UNNAMED.MINIGAME',       'Unnamed Minigame',     'Navneløst Miniaturespil'),
 ('DATA.UNNAMED.LOCATION',       'Unnamed Location',     'Navneløs Lokation'),
-('DATA.UNNAMED.ACHIEVEMENT',       'Unnamed Achievement',     'Navneløs Præstation')
+('DATA.UNNAMED.ACHIEVEMENT',    'Unnamed Achievement',  'Navneløs Præstation'),
 ('UI.DESCRIPTION_MISSING',      'Missing Description',  'Manglende Beskrivelse'),
-('ACHIEVEMENT.TUTORIAL_COMPLETE.TITLE',      'Tutorial Complete',  'Undervisningstime Fuldført');
-('ACHIEVEMENT.TUTORIAL_COMPLETE.DESCRIPTION',      'You have braved the migthy obstacles of the tutorial - well done!',  'Du har trodset de uoverkommelige forhindringer i undervisningstimen - flot klaret!');
+('ACHIEVEMENT.TUTORIAL_COMPLETE.TITLE', 'Tutorial Complete', 'Undervisningstime Fuldført'),
+('ACHIEVEMENT.TUTORIAL_COMPLETE.DESCRIPTION', 'You have braved the mighty obstacles of the tutorial - well done!', 'Du har trodset de uoverkommelige forhindringer i undervisningstimen - flot klaret!');
 
